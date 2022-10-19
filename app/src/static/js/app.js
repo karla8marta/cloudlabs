1c2c22fd86a8bc87a0b05e9068831f684b62173b
karla8marta <karla8marta@gmail.com>
2022-10-19 13:35:31 +0000

    lab2new

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="Add New Item Here"
+                    placeholder="Add Your New Item Here"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
